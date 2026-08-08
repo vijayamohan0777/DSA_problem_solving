e7b39a4bb65cc881067d162f0e10e926b6efd5bc
vijayamohan <vijayamohan0777@gmail.com>
2026-08-08 10:07:32 +0530

    Time: 1 ms (19.00%) | Memory: 46.5 MB (70.77%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -4,7 +4,8 @@ public class Solution {
     boolean isCyclic(ListNode head){
         ListNode slow= head;
         ListNode fast= head;
-        while( fast!=null && fast.next!=null){
+         if(head==null) return false;
+        while( fast.next!=null && fast.next.next!=null){
              slow=slow.next;
             fast=fast.next.next;
             if(slow==fast){
