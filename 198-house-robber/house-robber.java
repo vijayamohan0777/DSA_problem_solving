07c78a8760d539be5b091a097ec0b3f8b305f074
vijayamohan <vijayamohan0777@gmail.com>
2026-08-19 21:09:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (75.82%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,15 +1,34 @@
 class Solution {
-    int[] dp;
+     int[] dp;
+
+    int maxsum(int[] arr,int n){
+     if (dp==null){
+        dp=new int[n+1];
+        Arrays.fill(dp,-1);
+     }
+        if(n<=0){
+            return 0;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        int take=arr[n-1]+maxsum(arr,n-2);
+        int skip=maxsum(arr,n-1);
+
+         dp[n]= Math.max(take,skip);
+         return dp[n];
+    }
     public int rob(int[] nums) {
-        int n=nums.length;
-         dp=new int[n+1];
-         dp[0]=0;
-         dp[1]=nums[0];
-         for(int i=2;i<=n;i++){
-             dp[i]=Math.max(nums[i-1]+dp[i-2] ,dp[i-1]);
-         }
+    //     int n=nums.length;
+    //      dp=new int[n+1];
+    //      dp[0]=0;
+    //      dp[1]=nums[0];
+    //      for(int i=2;i<=n;i++){
+    //          dp[i]=Math.max(nums[i-1]+dp[i-2] ,dp[i-1]);
+    //      }
              
-     return dp[n];
+    //  return dp[n];
 
         // int older=0;
         // int oldest=0;
@@ -20,6 +39,8 @@ class Solution {
         //     older=current;
         // }
         // return current;
+        int n=nums.length;
+         return maxsum(nums, n);
 
     }
 }
