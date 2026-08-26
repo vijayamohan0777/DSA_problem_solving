ba5b23f14d614b48e58c15c6b8581b85be48cb23
vijayamohan <vijayamohan0777@gmail.com>
2026-08-26 16:36:23 +0530

    Time: 19 ms (13.70%) | Memory: 48.8 MB (52.05%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,43 +2,67 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m=nums1.length;
         int n=nums2.length;
-        int[] ans=new int[m+n];
+        // int[] ans=new int[m+n];
+
+      PriorityQueue<Integer> min=new PriorityQueue<>();
+      PriorityQueue<Integer> max=new PriorityQueue<>(Collections.reverseOrder());
+      
+      for(int x: nums1){
+        max.add(x);
+        min.add(max.poll());
+
+        if(min.size()>max.size()){
+            max.add(min.poll());
+        }
+      }
+        for(int y: nums2){
+        max.add(y);
+        min.add(max.poll());
+
+        if(min.size()>max.size()){
+            max.add(min.poll());
+        }
+      }
        
-        int l1=0,l2=0;
-        int i=0;
-        while(l1 <m && l2 <n){
-          if(nums1[l1]<nums2[l2]){
-            ans[i++]=nums1[l1];
-            l1++;
-          }else{
-            ans[i++]=nums2[l2];
-            l2++;
-          }
-        }
+       if(max.size()==min.size()){
+        return (max.peek()+min.peek())/2.0;
+       }
+       return max.peek();
+        // int l1=0,l2=0;
+        // int i=0;
+        // while(l1 <m && l2 <n){
+        //   if(nums1[l1]<nums2[l2]){
+        //     ans[i++]=nums1[l1];
+        //     l1++;
+        //   }else{
+        //     ans[i++]=nums2[l2];
+        //     l2++;
+        //   }
+        // }
 
-        if(l1==m && l2!=n){
-            for(int j=l2;j<n;j++){
-                ans[i++]=nums2[j];
-            }
-        }
+        // if(l1==m && l2!=n){
+        //     for(int j=l2;j<n;j++){
+        //         ans[i++]=nums2[j];
+        //     }
+        // }
          
          
-        if(l2==n && l1!=m){
-            for(int j=l1;j<m;j++){
-                ans[i++]=nums1[j];
-            }
-        }
+        // if(l2==n && l1!=m){
+        //     for(int j=l1;j<m;j++){
+        //         ans[i++]=nums1[j];
+        //     }
+        // }
 
-        int size=ans.length;
-        double median=0;
+        // int size=ans.length;
+        // double median=0;
 
-         if((m+n) %2 ==0){
-            median=ans[size >> 1]+ans[(size >> 1 )-1];
+        //  if((m+n) %2 ==0){
+        //     median=ans[size >> 1]+ans[(size >> 1 )-1];
 
-            return (double) median/2;
-         }else{
-            median=ans[size >> 1];
-            return (double) median;
-         }
+        //     return (double) median/2;
+        //  }else{
+        //     median=ans[size >> 1];
+        //     return (double) median;
+        //  }
     }
 }
