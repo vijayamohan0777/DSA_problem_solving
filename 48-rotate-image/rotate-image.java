655aae788f7b7c9b513bf055dff33f21e75c1a53
vijayamohan <vijayamohan0777@gmail.com>
2026-09-02 14:21:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (71.23%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        //Transpose the matrix
+
+        //by swap the  upper diagnol elements of matrix with lower diagnol element
+        // if i can use extra space like temp[][]
+        // temp[i][j]  = matrix[j][i];
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse the each row of  transposed matrix
+
+        for (int i = 0; i < n; i++) {
+            int start = 0;
+            int end = n - 1;
+            while (start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
