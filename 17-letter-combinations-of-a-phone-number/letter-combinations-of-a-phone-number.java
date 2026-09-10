3bea056ccec21c3609298a7e2573ddb04001b2c3
vijayamohan <vijayamohan0777@gmail.com>
2026-09-10 10:39:47 +0530

    Time: 3 ms (34.20%) | Memory: 49.4 MB (5.42%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,30 +1,32 @@
 class Solution {
-
-    void solve(String digits,int index,String currentletters,ArrayList<String> combinations,String[] map){
-          if(index == digits.length()){
-            combinations.add(currentletters);
+    void solve(int index, String currentstr, List<String> ans, String digits, String[] map) {
+        if (index == digits.length()) {
+            ans.add(currentstr);
             return;
-          }
+        }
 
-          String letters=map[digits.charAt(index) - '0'];
+        String letters = map[digits.charAt(index) - '0'];
 
+        for (int i = 0; i < letters.length(); i++) {
+            currentstr += letters.charAt(i);
+            solve(index + 1, currentstr, ans, digits, map);
+            currentstr=currentstr.substring(0,currentstr.length()-1);
 
-          for(int i=0;i<letters.length();i++){
-            solve(digits,index+1,currentletters+letters.charAt(i),combinations,map);
-          }
+            //or
+
+          //  solve(index+1,currentstr+letters.charAt(i),ans,digits,map);
+        }
     }
+
     public List<String> letterCombinations(String digits) {
-        String[] map = { "", "", "abc", "def",
-                "ghi", "jkl", "mno",
-                "pqrs", "tuv", "wxyz" };
+        String[] map = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        List<String> ans = new ArrayList<>();
 
-                ArrayList<String> combinations=new ArrayList<>();
+        String currentstr = "";
+        int size = 0;
 
-                String currentletters="";
-                int index=0;
+        solve(size, currentstr, ans, digits, map);
 
-                solve(digits,index,currentletters,combinations,map);
-
-                return combinations;
+        return ans;
     }
 }
