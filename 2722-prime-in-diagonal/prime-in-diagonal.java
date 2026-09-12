3fe41c78b363b6c702725f29ea896e23d7d37cf0
vijayamohan <vijayamohan0777@gmail.com>
2026-09-12 14:04:56 +0530

    Time: 4 ms (77.13%) | Memory: 82.5 MB (57.21%) - LeetSync

--- 2722-prime-in-diagonal/prime-in-diagonal.java
+++ 2722-prime-in-diagonal/prime-in-diagonal.java
@@ -10,7 +10,7 @@ class Solution {
             return false;
         }
 
-        for(int i=3;i<=Math.sqrt(n);i++){
+        for(int i=3;i<=Math.sqrt(n);i+=2){
             if(n%i==0){
                 return false;
             }
