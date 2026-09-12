7720b42d413ff500e681d4df3953ac3117c85f53
vijayamohan <vijayamohan0777@gmail.com>
2026-09-12 15:54:11 +0530

    Time: 1 ms (81.76%) | Memory: 43.6 MB (79.18%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -14,63 +14,43 @@
  * }
  */
 class Solution {
-    List<List<Integer>> ans=new ArrayList<>();
-    
-    void solve(TreeNode root, Queue<TreeNode> q){
-        int level=0;
-         while(!q.isEmpty()){
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        boolean leftToright = true;
 
-            int size=q.size();
-            List<Integer> list=new ArrayList<>();
-         if(level % 2==0){
-            for(int i=0;i<size;i++){
-                 TreeNode temp=q.poll();
-                  list.add(temp.val);
-                   
-                if(temp.left !=null){
+        while (!q.isEmpty()) {
+
+            int size = q.size();
+            LinkedList<Integer> list = new LinkedList<>();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode temp = q.poll();
+
+                if (leftToright == true) {
+                    list.addLast(temp.val);
+                } else {
+                    list.addFirst(temp.val);
+                }
+
+                if (temp.left != null) {
                     q.offer(temp.left);
                 }
-                if(temp.right != null){
+
+                if (temp.right != null) {
                     q.offer(temp.right);
                 }
             }
             ans.add(new ArrayList<>(list));
-         }else{
 
-            Stack<Integer> stack=new Stack<>();
-
-            for(int i=0;i<size;i++){
-                 TreeNode temp=q.poll();
-                  stack.push(temp.val);
-                   
-                if(temp.left !=null){
-                    q.offer(temp.left);
-                }
-                if(temp.right != null){
-                    q.offer(temp.right);
-                }
-            }
-
-                while(!stack.isEmpty()){
-                    list.add(stack.pop());
-                }
-
-             ans.add(new ArrayList<>(list));
-         }
-            
-             level++;
-         }
-    }
-
-    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Queue<TreeNode> q=new LinkedList<>();
-
-        if(root == null){
-            return ans;
+            leftToright = !leftToright; // for zigzag order
         }
-        q.offer(root);
-       
-       solve(root,q);
+
         return ans;
     }
 }
