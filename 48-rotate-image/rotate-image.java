fc060fa8f31f00d0a29fcabc370d04fd7723979e
vijayamohan <vijayamohan0777@gmail.com>
2026-09-12 12:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (16.50%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,32 +1,31 @@
 class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
+    public void rotate(int[][] mat) {
+        int n=mat.length;
+      
+      //transpose by swap upper diagonal element with lower diagonal element
 
-        //Transpose the matrix
-
-        //by swap the  upper diagnol elements of matrix with lower diagnol element
-        // if i can use extra space like temp[][]
-        // temp[i][j]  = matrix[j][i];
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {  //j=i+1   is important for upper diagnol transpose
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
+      for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            int temp=mat[i][j];
+            mat[i][j]=mat[j][i];
+            mat[j][i]=temp;
         }
+      }
 
-        // Reverse the each row of  transposed matrix
+      //reverse each row
 
-        for (int i = 0; i < n; i++) {
-            int start = 0;
-            int end = n - 1;
-            while (start < end) {
-                int temp = matrix[i][start];
-                matrix[i][start] = matrix[i][end];
-                matrix[i][end] = temp;
-                start++;
-                end--;
-            }
+      for(int i=0;i<n;i++){
+        int start=0;
+        int end=n-1;
+
+        while(start<end){
+            int temp=mat[i][start];
+            mat[i][start]=mat[i][end];
+            mat[i][end]=temp;
+            start++;
+            end--;
         }
+      }
+      
     }
 }
