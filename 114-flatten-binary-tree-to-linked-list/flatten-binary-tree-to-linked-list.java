3eb99dfc2fee86fbd0f7eefd7b5df0bf01e35c21
vijayamohan <vijayamohan0777@gmail.com>
2026-09-18 13:08:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (85.30%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -13,32 +13,56 @@
  *     }
  * }
  */
+
+//  class Solution {
+
+//     List<Integer> prelist = new ArrayList<>();
+
+//     void preorder(TreeNode root) {
+
+//         if (root == null) {
+//             return;
+//         }
+
+//         prelist.add(root.val);
+//         preorder(root.left);
+//         preorder(root.right);
+//     }
+
+//     public void flatten(TreeNode root) {
+//         if (root == null) {
+//             return;
+//         }
+//         preorder(root);
+//         TreeNode temp = root;
+//         for (int i = 1; i < prelist.size(); i++) {
+//             temp.left = null;
+//             TreeNode nn = new TreeNode(prelist.get(i));
+//             temp.right = nn;
+//             temp = temp.right;
+//         }
+//     }
+// }
+
+// my solution is O(n)
+
+//this is O(h)  --> optimal
+//use reverse preorder
 class Solution {
+    TreeNode prev = null;
 
-    List<Integer> prelist=new ArrayList<>();
-    void preorder(TreeNode root){
+    public void flatten(TreeNode root) {
 
-        if(root==null){
+        if (root == null) {
             return ;
         }
 
-       prelist.add(root.val);
-       preorder(root.left);
-       preorder(root.right);
-    }
-    
+        flatten(root.right);
+        flatten(root.left);
 
-    public void flatten(TreeNode root) {
-        if(root == null){
-        return;
-       }
-        preorder(root);
-         TreeNode temp=root;
-        for(int i=1;i<prelist.size();i++){
-              temp.left=null;
-              TreeNode nn=new TreeNode(prelist.get(i));
-              temp.right=nn;
-              temp=temp.right;
-        }
+        root.right = prev;
+        root.left = null;
+        //important
+        prev = root;
     }
 }
